b42dc4010f3a54f74db79534ce654f924b6d0615
opchaves <opaulochaves@gmail.com>
2025-08-06 22:26:38 -0300

    config production

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,3 @@
-// import { reactRouter } from "@react-router/dev/vite";
-// import tailwindcss from "@tailwindcss/vite";
-// import { defineConfig } from "vite";
-// import tsconfigPaths from "vite-tsconfig-paths";
-//
-// export default defineConfig(({ isSsrBuild }) => ({
-//   build: {
-//     rollupOptions: isSsrBuild
-//       ? {
-//           input: "./server/app.ts",
-//         }
-//       : undefined,
-//   },
-//   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-// }));
-
 import { reactRouter } from "@react-router/dev/vite";
 import { cloudflare } from "@cloudflare/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
